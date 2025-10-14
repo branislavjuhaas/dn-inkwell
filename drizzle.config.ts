86402fd492bbd8ff8fdad0406afd59bf1c2d075a
Branislav Juhás <juhas.branislaw@gmail.com>
2025-10-14 11:04:31 +0200

    chore: clean up code structure and remove unused code blocks

--- drizzle.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "drizzle-kit";
-
-export default {
-  schema: "./server/db/schema.ts",
-  out: "./server/db/migrations",
-  dialect: "sqlite",
-  verbose: true,
-  strict: true,
-} satisfies Config;
